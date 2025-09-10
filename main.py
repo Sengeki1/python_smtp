9f0c20b69721c32675eb79b6d03753628f8aa82e
Sengeki1 <gamersengeki@gmail.com>
2025-09-10 13:51:06 -0100

    email format

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import smtplib, ssl
 import os
 from dotenv import load_dotenv
+from email.message import EmailMessage
 
 load_dotenv()
 
@@ -12,6 +13,16 @@ sender = os.getenv("EMAIL_SENDER")
 app_password = os.getenv("APP_PASSWORD")
 receiver = os.getenv("EMAIL_RECEIVER")
 
+body = """
+Hello from Python
+"""
+
+email = EmailMessage()
+email['From'] = sender
+email['To'] = receiver
+email['Subject'] = "Test"
+email.set_content(body)
+
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(sender, app_password)
-    server.sendmail(sender, receiver, "Subject: test\r\n\r\nHello from Python")
+    server.sendmail(sender, receiver, email.as_string())
